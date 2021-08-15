282e64755a9d7cc9cfcf1652c26785b5f46fff76
Gibran Polonsy <gibran@Gibrans-MacBook-Pro.local>
2021-08-15 08:16:57 -0600

    ADD user model and manager

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email(self):
+        """Test for create a user with email and password"""
+        email = 'email@email.com'
+        password = 'Test123!'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalizaed(self):
+        """Test email user normalize"""
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_user_invalid_email(self):
+        """Test for vaidate email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_super_user(self):
+        """Test create super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
